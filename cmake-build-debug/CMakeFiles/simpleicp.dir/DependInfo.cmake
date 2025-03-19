c15abb67c856f03eec6307e92a9aab9f7a5d2c5b
Stuprosur <2511002562@qq.com>
2025-03-19 16:03:35 +0800

    Add .gitignore file

---
+++ cmake-build-debug/CMakeFiles/simpleicp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dhc/CLionProjects/TaskOpt/PointCloudRe/simpleICP/c++/src/simpleicp-cli.cpp" "/home/dhc/CLionProjects/TaskOpt/PointCloudRe/simpleICP/c++/cmake-build-debug/CMakeFiles/simpleicp.dir/src/simpleicp-cli.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dhc/CLionProjects/TaskOpt/PointCloudRe/simpleICP/c++/cmake-build-debug/CMakeFiles/simpleicp_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/simpleicp_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libsimpleicp_lib.a"
+)
